3c727f6ba7c8347c53959e506fe7c5647db6417c
IAmRelative314 <s405400@ocsb.local>
2017-03-03 10:54:03 -0500

    BallProject

---
+++ Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text finishText;
+    public Text endText;
+
+    private Rigidbody rb;
+    private int count;
+    private int end;
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        end = 0;
+        endText.text = "";
+    }
+   
+     void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float movevertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, movevertical);
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+
+        if (other.gameObject.CompareTag("Cube"))
+        {
+            other.gameObject.SetActive(false);
+            endText.text = "You is has WIN !!";
+
+        }
+            
+}
+void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 20)
+        {
+            winText.text = "You is has WIN !!";
+        }
+    }
+
+
+}
